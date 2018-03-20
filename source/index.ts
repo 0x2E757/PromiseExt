7c6a025c00a06d157b7dd14d4c0e6057ba56fab3
0x2E757 <0x2E757@gmail.com>
2018-03-20 13:54:55 +0200

    Reimplemented action execution process to be more like native Promise.

--- source/index.ts
+++ source/index.ts
@@ -1,24 +1,24 @@
-export enum State { Scheduled, Started, Canceled, Fulfilled, Rejected }
+export enum State { Scheduled, Started, Finished, Canceled }
 
 export type InitialAction = (resolve: Function, reject: Function) => any;
+
 export type Action = (value: any) => any;
 export enum ActionType { Resolver = 1, Rejector = 2, Finalizer = 3 }
 export type ActionStack = { type: ActionType, action: Action }[];
 
-export type FailHandler = (error: any) => any;
-export type SuccessHandler = (result: any) => any;
+export type UnhandledRejectionHandler = (error: any) => any;
 
 export class PromiseExt {
 
-    public static onSuccess: SuccessHandler | null = null;
-    public static onFail: FailHandler = (error: any) => console.error("Unhandled promise error", error);
+    public static unhandledRejectionHandler: UnhandledRejectionHandler = (error: any) => {
+        console.error("Unhandled promise rejection", error);
+    }
 
     public state: State = State.Scheduled;
     public get isScheduled(): boolean { return this.state === State.Scheduled; }
     public get isStarted(): boolean { return this.state === State.Started; }
+    public get isFinished(): boolean { return this.state === State.Finished; }
     public get isCanceled(): boolean { return this.state === State.Canceled; }
-    public get isFulfilled(): boolean { return this.state === State.Fulfilled; }
-    public get isRejected(): boolean { return this.state === State.Rejected; }
     public cancel = () => this.state = State.Canceled;
 
     private initialAction: InitialAction;
@@ -26,62 +26,99 @@ export class PromiseExt {
     private index: number = 0;
     private parent: PromiseExt | null = null;
 
-    private result: any = undefined;
-    private hasError: boolean = false;
+    private result!: any;
+    private hasError!: boolean;
 
     constructor(initialAction: InitialAction) {
         this.initialAction = initialAction;
         setTimeout(this.start);
     }
 
-    private start = () => {
+    private resolve = (value: any): void => {
+        this.result = value;
+        this.hasError = false;
+        this.exec();
+    }
+
+    private reject = (value: any): void => {
+        this.result = value;
+        this.hasError = true;
+        this.exec();
+    }
+
+    private start = (): void => {
         if (this.isScheduled) {
             this.state = State.Started;
-            try { this.initialAction(this.onThen, this.onCatch); }
-            catch (error) { this.onCatch(error); }
+            try {
+                this.initialAction(this.resolve, this.reject);
+            } catch (error) {
+                this.reject(error);
+            }
         }
     }
 
-    private handleAction = (action: { type: ActionType, action: Action }, value: any) => {
-        try {
-            const result = action.action(value);
-            if (action.type === ActionType.Finalizer) {
-                this.hasError ? this.onCatch(this.result) : this.onThen(this.result);
-            } else {
-                this.result = result;
+    private onThen = (action: Action): void => {
+        if (this.hasError === false) {
+            try {
+                this.result = action(this.result);
                 this.hasError = false;
-                result instanceof PromiseExt ? this.awaitChild(result) : this.onThen(result);
+            } catch (error) {
+                this.result = error;
+                this.hasError = true;
             }
-        } catch (error) { 
+        }
+    }
+
+    private onCatch = (action: Action): void => {
+        if (this.hasError === true) {
+            try {
+                this.result = action(this.result);
+                this.hasError = false;
+            } catch (error) {
+                this.result = error;
+                this.hasError = true;
+            }
+        }
+    }
+
+    private onFinally = (action: Action): void => {
+        try {
+            action(undefined);
+        } catch (error) {
             this.result = error;
             this.hasError = true;
-            this.onCatch(this.result);
         }
     }
 
-    private exec = (value: any, actionType: ActionType, finalizer: Function | null) => {
+    private exec = (): void => {
         if (this.isCanceled) return;
         while (this.index < this.actions.length) {
             const index = this.index++;
-            if (this.actions[index].type & actionType) {
-                return this.handleAction(this.actions[index], value);
+            switch (this.actions[index].type) {
+                case ActionType.Resolver: {
+                    this.onThen(this.actions[index].action);
+                    break;
+                }
+                case ActionType.Rejector: {
+                    this.onCatch(this.actions[index].action);
+                    break;
+                }
+                case ActionType.Finalizer: {
+                    this.onFinally(this.actions[index].action);
+                    break;
+                }
+            }
+            if (this.result instanceof PromiseExt) {
+                this.result.then(this.resolve, this.reject).parent = this;
+                return;
             }
         }
-        if (finalizer) finalizer(value);
-    }
-
-    private onThen = (value: any): any => {
-        const finalizer = this.parent ? this.parent.onThen : PromiseExt.onSuccess;
-        this.exec(value, ActionType.Resolver, finalizer);
-    }
-
-    private onCatch = (value: any): any => {
-        const finalizer = this.parent ? this.parent.onCatch : PromiseExt.onFail;
-        this.exec(value, ActionType.Rejector, finalizer);
-    }
-
-    private awaitChild = (promise: PromiseExt) => {
-        promise.then(this.onThen, this.onCatch).parent = this;
+        if (this.isStarted) {
+            this.state = State.Finished;
+            if (this.hasError && this.parent === null) {
+                PromiseExt.unhandledRejectionHandler(this.result);
+            }
+        }
     }
 
     public then = (action: Action, rejector?: Action): this => {
@@ -94,7 +131,7 @@ export class PromiseExt {
         return rejector ? this.catch(rejector) : this;
     }
 
-    public finally = (action: Action, rejector?: Action) => {
+    public finally = (action: Action, rejector?: Action): this => {
         this.actions.push({ type: ActionType.Finalizer, action });
         return rejector ? this.catch(rejector) : this;
     }
